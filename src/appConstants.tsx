150ff925d4980a178241c14ad788d38c9e7d76bd
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-07-02 21:39:35 -0500

    first deploy

--- src/appConstants.tsx
+++ src/appConstants.tsx
@@ -1,4 +1,4 @@
-const realApiUrl = 'api.example.com';
+const realApiUrl = 'chattus-back.onrender.com';
 const devApiUrl = 'localhost:8000';
 
 export const apiUrl = import.meta.env.MODE === 'development' ? devApiUrl : realApiUrl;
