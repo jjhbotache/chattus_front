75f0e32b52fa764522a93125d76f9043d13f0611
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-07-07 16:40:53 -0500

    delete hide function

--- src/main.tsx
+++ src/main.tsx
@@ -10,66 +10,6 @@ import { hideAll, showAll } from './helpers/visibilityFucntions'
 
 
 
-document.addEventListener('visibilitychange', ()=>{
-  console.log('Visibility has changed');
-  document.visibilityState === 'visible'
-    ?showAll()
-    :hideAll();
-});
-
-window.addEventListener('blur', ()=>{
-  // console.log('Window is now blur');
-  hideAll();
-});
-
-window.addEventListener('focus', ()=>{
-  // console.log('Window is now focus');
-  showAll();
-});
-
-window.addEventListener('keydown',(event)=>{
-  // console.log(event.key);
-  if (event.key.includes("Audio")) {
-    hideAll();
-  }
-  
-  if (event.key.includes("F")) {
-    hideAll();
-  }
-  
-  if (event.key === 'Meta' || event.key === 'Windows') {
-      console.log('La tecla Command (Mac) ha sido presionada.');
-      hideAll();
-  }
-});
-
-window.addEventListener('keyup', () => {
-  showAll();
-});
-
-window.addEventListener('beforeunload', () => {
-  hideAll();
-})
-
-window.addEventListener('unload', () => {
-  hideAll();
-})
-
-window.addEventListener('pagehide', () => {
-  hideAll();
-}) 
-
-window.addEventListener('pageshow', () => {
-  showAll();
-})
-
-window.addEventListener('click', () => {
-  showAll();
-})
-
-
-
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
     <ToastContainer
