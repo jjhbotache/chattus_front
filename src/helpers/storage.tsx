bed496f165d043948810318c086779197087837f
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-07-07 16:05:35 -0500

    making a bigger store

--- src/helpers/storage.tsx
+++ src/helpers/storage.tsx
@@ -1,5 +1,5 @@
 
-export const maxSizeInBytes = .005 * (1024 * 1024 * 1024); // 5MB max size of files in variable
+export const maxSizeInBytes = .1 * (1024 * 1024 * 1024); // 5MB max size of files in variable
 
 const maxWebsocketMessageSize = 9083009 // 9MB buffer size of websocket message
 
